d589b32379aad91f6dae252b286c3f831fc5c052
Thomas Noriega <TRN@BeyondTRN.local>
2017-05-25 14:30:18 -0700

    Remove copy/paste tools from dictools

--- chemproject/dictools.py
+++ chemproject/dictools.py
@@ -7,15 +7,6 @@ cs = ChemSpider('0201ba66-585d-4135-9e6b-d28ba4724fcf')
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 from inspect import getmembers, isfunction
-import subprocess
-
-def write_to_clipboard(output):
-    process = subprocess.Popen(
-        'pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
-    process.communicate(output.encode('utf-8'))
-
-def read_from_clipboard():
-    return subprocess.check_output('pbpaste', env={'LANG': 'en_US.UTF-8'}).decode('utf-8')
 
 def link_to_soup(link):
     '''
