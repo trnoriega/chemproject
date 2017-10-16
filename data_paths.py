c16f222310a103a5f0bca6cbb6ce76ebc01ca2be
Thomas Noriega <trnoriega@users.noreply.github.com>
2017-10-16 12:49:57 -0700

    Add importable generic data_paths file that works on all workbooks in case datadump folder location is changed.

---
+++ data_paths.py
@@ -0,0 +1,10 @@
+import os.path as path
+
+# Set the base path for where you want to store intermediate data dums here
+# Current BASE_PATH is an example of how I set it up. 
+
+BASE_PATH = path.join(path.expanduser('~'),
+                     'Dropbox',
+                     'personal',
+                     'data_dumps',
+                     'chem_project')
